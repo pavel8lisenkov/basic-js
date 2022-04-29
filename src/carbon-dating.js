c95506fa43b8d9513e3d61d6eb1ffb5b394dd5d9
Pasha <pavel8lisenkov@yandex.ru>
2022-04-30 00:57:12 +0400

    complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity * 0 !== 0 || +sampleActivity > 15 || sampleActivity <= 0) {
+    return false;
+  }
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+
+  let t = ((Math.log(MODERN_ACTIVITY / sampleActivity)) / k);
+
+  return Math.ceil(t);
+
 }
 
 module.exports = {
