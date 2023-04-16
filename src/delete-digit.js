9936a6a64251b5e8ccb4ad23161f2f9503b7c03a
Pasha <pavel8lisenkov@yandex.ru>
2023-04-16 21:28:55 +0400

    feat: completed tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const digits = String(n).split('').map(Number)
+  return Math.max(...digits.map((digit, i) => { let res = digits.slice(); res.splice(i, 1); return Number(res.join('')) }))
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  return str.replace(/([a-z])\1*/g, (str) => str.length > 1 ? str.length + str[0] : str[0]
+  );
 }
 
 module.exports = {
