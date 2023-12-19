954e4d50030a3b4c409d692ea13befd83a872d30
Pasha <pavel8lisenkov@yandex.ru>
2023-12-20 02:54:06 +0400

    What season task completed

--- src/what-season.js
+++ src/what-season.js
@@ -2,25 +2,27 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
 function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+
+  try { date.getTime() } catch {
+    throw new Error('Invalid date!');
+  }
+
   let month = date.getMonth();
 
-  if (!arguments.length) {
-    return 'Unable to determine the time of year!';
-  } else if (!date.getTime) {
-    throw new Error('Invalid date!');
-  } else if (typeof date.getMonth() !== 'number') {
-    throw new Error('Invalid date!');
-  } else if (month === 0 || month === 1 || month === 11) {
+  if (month === 0 || month === 1 || month === 11) {
     return 'winter';
   } else if (month >= 2 && month <= 4) {
     return 'spring';
